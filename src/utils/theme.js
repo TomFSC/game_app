0a038030fc2ac999a0e5e6e19929b399ba89ac54
Thomas FAISCA <faisca.thomas@gmail.com>
2023-02-26 14:36:54 +0100

    🚧 F01: Create Home & add theme.js

---
+++ src/utils/theme.js
@@ -0,0 +1,59 @@
+const theme = {
+  colors: {
+    lapisLazuli: "#416788",
+    platinum: "#D8DBE2",
+    glaucous: "#7389AE",
+    frenchGray: "#B5BAD0",
+    tiffanyBlue: "#81D2C7",
+    dark: "#333333",
+    white: "#FFF",
+    ghostWhite: "#F9F7FF",
+    platinumGrey: "#DBDADD",
+    silver: "#C2C1C2",
+  },
+  fonts: {
+    size: {
+      P0: "11px",
+      P1: "18px",
+      P2: "20px",
+      P3: "30px",
+      P4: "40px",
+      P5: "50px",
+    },
+  },
+  spacing: {
+    null: "0",
+    xxs: "5px",
+    xs: "10px",
+    s: "15px",
+    m: "20px",
+    l: "25px",
+    xl: "50px",
+    xxl: "100px",
+    xxxl: "150px",
+    xtra: "200px",
+  },
+  dimensions: {
+    percent: { small: "12%", medium: "50%", tall: "88%", max: "100%" },
+    pixels: { small: "50px", medium: "200px", large: "300px" },
+  },
+  scale: {
+    small: 1.5,
+    medium: 2,
+    big: 3,
+  },
+  boxShadow: {
+    medium: "5px 5px 10px #C2C1C2",
+    softSilver: "0 8px 5px #C2C1C2",
+    softPurple: "2px 2px 10px #5430A7",
+    mediumPurple: "5px 5px 20px #5430A7",
+  },
+  radius: {
+    xtraSoft: "5px",
+    soft: "20px",
+    rounded: "35px",
+    circle: "50%",
+  },
+}
+
+export default theme
